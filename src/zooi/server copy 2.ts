b0f4db6b5780ca6ddde9c8790348053a47e8a2e2
Rudy Berends <rudy.berends@visolity.nl>
2024-10-08 15:05:16 +0000

    fix: remove

--- src/zooi/server copy 2.ts
+++
@@ -1,8 +0,0 @@
-import logger from './utils/troxorlogger'; // Logger utility
-import { processMessages } from './serviceBusSubscriber';
-
-
-// Start processing messages
-processMessages().catch(err => {
-    logger.error("Error in processMessages: %s", err);
-});
